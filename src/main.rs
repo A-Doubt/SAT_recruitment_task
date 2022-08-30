0841ad5b45c5b706f07071f9f3f69a70b4b14f40
A-Doubt <adoubt2@gmail.com>
2022-08-30 22:50:45 +0200

    Implemented /calculateDieselUsageForDistance. Parameters provided in the query are validated.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,37 @@
+use chrono::Datelike;
 use rand::Rng;
 use regex::Regex;
 
+
 #[macro_use] extern crate rocket;
 
+pub struct Params {
+	year_of_production: u32,
+	distance: u32,
+	fuel_usage_per_100_km: f32,
+}
+
 // Task stated "Dissel", but I think it was a typo, so changed to "Diesel".
-#[get("/calculateDieselUsageForDistance")]
-fn fuel_usage() -> String {
-	String::from("fuel_usage endpoint")
+#[allow(non_snake_case)] // for query params
+#[get("/calculateDieselUsageForDistance?<distance>&<yearOfProduction>&<fuelUsagePer100KM>")]
+fn fuel_usage(distance: u32, yearOfProduction: u32, fuelUsagePer100KM: f32) -> String {
+	
+	let fuel_consumption_params:Params = Params {
+		year_of_production: yearOfProduction,
+		distance,
+		fuel_usage_per_100_km: fuelUsagePer100KM,
+	};
+
+	let params_valid:bool = validate_fuel_consumption_params(&fuel_consumption_params);
+
+	if !params_valid {
+		return String::from("The provided query parameters are not valid.");
+	}
+
+	let result: f32 = 
+		calculate_fuel_usage(fuel_consumption_params.distance, fuel_consumption_params.fuel_usage_per_100_km);
+
+	format!("{}", result)
 }
 
 #[get("/probabilityOfUnitInjectorFail?<vin>")]
@@ -26,9 +51,33 @@ fn injector_fail(vin: String) -> String {
 #[launch]
 fn rocket() -> _ {
 	rocket::build()
-		.mount("/", routes![fuel_usage, injector_fail])
+	.mount("/", routes![fuel_usage, injector_fail])
 }
 
+
+fn validate_fuel_consumption_params(params: &Params) -> bool {
+	let current_year: i32 = chrono::Utc::now().year();
+
+	if params.distance <= 0 {
+		return false;
+	}
+	if params.fuel_usage_per_100_km <= 0.0 {
+		return false;
+	}
+
+	// some arbitrary year of production
+	if params.year_of_production < 1900 || params.year_of_production > current_year as u32 {
+		return false;
+	}
+
+	true
+}
+
+fn calculate_fuel_usage(distance: u32, fuel_usage_per_100_km: f32) -> f32 {
+	let result: f32 = (fuel_usage_per_100_km * distance as f32) / 100 as f32;
+
+	result
+}
 
 fn validate_vin(vin: String) -> bool {
 	let regex = Regex::new("^[a-zA-Z0-9]{11,17}$").unwrap();
